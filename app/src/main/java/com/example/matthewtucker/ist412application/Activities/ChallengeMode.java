f4a08ac168d361ad43374638c9cc29d1e4571ae9
Morgan Radic <radic44morgan@gmail.com>
2018-04-23 15:54:22 -0400

    moved challenge mode class

--- app/src/main/java/com/example/matthewtucker/ist412application/Activities/ChallengeMode.java
+++ app/src/main/java/com/example/matthewtucker/ist412application/Activities/ChallengeMode.java
@@ -1,4 +1,4 @@
-package com.example.matthewtucker.ist412application;
+package com.example.matthewtucker.ist412application.Activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -6,6 +6,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+
+import com.example.matthewtucker.ist412application.R;
 
 public class ChallengeMode extends AppCompatActivity {
 
